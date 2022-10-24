c73b2df27dcce44c1795d508f2808b87937ea97d
IffatNur <iffatnoorshad00@gmail.com>
2022-10-25 00:16:29 +0600

    route link added

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const courses = require('./data/course.json');
+
 app.use(cors());
 
 app.get('/', (req,res)=>{
     res.send('edutech running');
+})
+
+app.get('/courses', (req,res) =>{
+    res.send(courses);
+})
+
+app.get('/courses/:id', (req,res)=>{
+    const id = req.params.id;
+    const course = courses.find(crs => crs.id == id);
+    res.send(course);
 })
 
 app.listen(port, ()=>{
